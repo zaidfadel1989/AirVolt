b89707a24aafa5e6e627e82ea3bdd18c08013365
zaidfadel1989 <zaid.fadel89@gmail.com>
2025-12-16 18:50:44 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
-  base: "/AirVolt/"
+  base: "/"  // Change from "/AirVolt/"
 })
